d821207805224a71414971bddf3dcfb697e3d88d
grze2000 <programista333@gmail.com>
2020-11-12 21:53:37 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="app">
+    <h1>Time Progress</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {}
+}
+</script>
+
+<style lang="scss">
+html, body {
+  height: 100%;
+}
+body {
+  margin: 0;
+}
+.app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  background-color: #E5E5E5;
+  height: 100%;
+}
+</style>
